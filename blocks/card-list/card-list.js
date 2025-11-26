dc00ecc5278c803703fa6006d7fa695fa03b6791
fansan6190 <fanindra.saini@axeno.co>
2025-11-26 02:38:59 -0800

    blocks/card-list/

--- blocks/card-list/card-list.js
+++ blocks/card-list/card-list.js
@@ -1,7 +1,5 @@
-import {
-  createOptimizedPicture,
-  moveInstrumentation,
-} from "../../scripts/aem.js";
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   console.log("check1");
