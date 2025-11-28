01f6392571a3afc288186f04e87e16ad16ff95ea
fansan6190 <fanindra.saini@axeno.co>
2025-11-27 23:27:00 -0800

    blocks/hero-full-width/_hero-full-width.js

--- blocks/hero-full-width/hero-full-width.js
+++ blocks/hero-full-width/hero-full-width.js
@@ -2,33 +2,27 @@ import { createOptimizedPicture } from "../../scripts/aem.js";
 import { moveInstrumentation } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
-  const backgroundVideoSource = block.querySelector(
+  const backgroundVideo = block.querySelector(
     '[data-aue-prop="backgroundVideo"]'
-  );
-  const backgroundPosterAlt = block.querySelector(
-    '[data-aue-prop="backgroundPosterAlt"]'
   );
   const title = block.querySelector('[data-aue-prop="title"]');
   const description = block.querySelector('[data-aue-prop="description"]');
-  const ctasContainer = block.querySelector('[data-aue-model="ctas"]');
-
-  const primaryCtaLabel = ctasContainer?.querySelector(
+  const primaryCtaLink = block.querySelector(
+    '[data-aue-prop="primaryCtaLink"]'
+  );
+  const primaryCtaLabel = block.querySelector(
     '[data-aue-prop="primaryCtaLabel"]'
   );
-  const primaryCtaHref = ctasContainer?.querySelector(
-    '[data-aue-prop="primaryCtaHref"]'
+  const secondaryCtaLink = block.querySelector(
+    '[data-aue-prop="secondaryCtaLink"]'
   );
-  const secondaryCtaLabel = ctasContainer?.querySelector(
+  const secondaryCtaLabel = block.querySelector(
     '[data-aue-prop="secondaryCtaLabel"]'
-  );
-  const secondaryCtaHref = ctasContainer?.querySelector(
-    '[data-aue-prop="secondaryCtaHref"]'
   );
 
   block.innerHTML = "";
   block.classList.add("parallax-child-2", "hero-in-view");
   block.setAttribute("data-media-type", "videoTypeSelected");
-  block.setAttribute("aria-hidden", "true");
 
   const coverDiv = document.createElement("div");
   coverDiv.classList.add("hero-full-width__cover");
@@ -38,42 +32,36 @@ export default function decorate(block) {
   backgroundDiv.classList.add("hero-full-width__background");
   block.append(backgroundDiv);
 
-  const backgroundWrapper = document.createElement("div");
-  backgroundWrapper.classList.add(
+  const backgroundWrapperDiv = document.createElement("div");
+  backgroundWrapperDiv.classList.add(
     "hero-full-width__background-wrapper",
     "zoom-out"
   );
-  backgroundDiv.append(backgroundWrapper);
+  backgroundDiv.append(backgroundWrapperDiv);
 
-  if (backgroundVideoSource) {
-    const video = document.createElement("video");
-    video.classList.add("hero-full-width__background-video");
-    video.setAttribute("aria-hidden", "true");
-    video.setAttribute("playsinline", "");
-    video.setAttribute("muted", "");
-    video.setAttribute("loop", "");
-    video.setAttribute("autoplay", "");
+  if (backgroundVideo) {
+    const videoElem = document.createElement("video");
+    videoElem.classList.add("hero-full-width__background-video");
+    videoElem.setAttribute("playsinline", "");
+    videoElem.setAttribute("muted", "");
+    videoElem.setAttribute("loop", "");
+    videoElem.setAttribute("autoplay", "");
 
-    const source = document.createElement("source");
-    source.setAttribute("src", backgroundVideoSource.textContent.trim());
-    source.setAttribute("type", "video/mp4");
-    moveInstrumentation(backgroundVideoSource, source);
-    video.append(source);
-    backgroundWrapper.append(video);
+    const sourceElem = document.createElement("source");
+    sourceElem.setAttribute("type", "video/mp4");
+    sourceElem.src = backgroundVideo.href || backgroundVideo.textContent.trim();
+    videoElem.append(sourceElem);
+    moveInstrumentation(backgroundVideo, sourceElem);
+    backgroundWrapperDiv.append(videoElem);
   }
 
-  const img = document.createElement("img");
-  img.setAttribute("loading", "lazy");
-  img.classList.add("hero-full-width__background-poster");
-  img.style.display = "none";
-  img.setAttribute("aria-hidden", "true");
-  if (backgroundPosterAlt) {
-    img.setAttribute("alt", backgroundPosterAlt.textContent.trim());
-    moveInstrumentation(backgroundPosterAlt, img);
-  } else {
-    img.setAttribute("alt", "Background poster image");
-  }
-  backgroundWrapper.append(img);
+  const posterImg = document.createElement("img");
+  posterImg.setAttribute("alt", "Background poster image");
+  posterImg.setAttribute("loading", "lazy");
+  posterImg.classList.add("hero-full-width__background-poster");
+  posterImg.style.display = "none";
+  posterImg.setAttribute("aria-hidden", "true");
+  backgroundWrapperDiv.append(posterImg);
 
   const contentDiv = document.createElement("div");
   contentDiv.classList.add("hero-full-width__content");
@@ -84,8 +72,8 @@ export default function decorate(block) {
   contentDiv.append(slideWrap1);
 
   const slideUp1 = document.createElement("div");
-  slideUp1.setAttribute("data-slide-type", "slide-up");
   slideUp1.classList.add("slide-up");
+  slideUp1.setAttribute("data-slide-type", "slide-up");
   slideWrap1.append(slideUp1);
 
   if (title) {
@@ -111,33 +99,32 @@ export default function decorate(block) {
   contentDiv.append(slideWrap2);
 
   const slideUp2 = document.createElement("div");
-  slideUp2.setAttribute("data-slide-type", "slide-up");
   slideUp2.classList.add("slide-up");
+  slideUp2.setAttribute("data-slide-type", "slide-up");
   slideWrap2.append(slideUp2);
 
   const ctasDiv = document.createElement("div");
   ctasDiv.classList.add("hero-full-width__content--ctas");
-  if (ctasContainer) {
-    moveInstrumentation(ctasContainer, ctasDiv);
-  }
   slideUp2.append(ctasDiv);
 
-  if (primaryCtaLabel && primaryCtaHref) {
-    const primaryLink = document.createElement("a");
-    primaryLink.classList.add("cta", "cta__secondary", "primaryCta");
-    primaryLink.setAttribute("target", "_self");
-    primaryLink.setAttribute("data-palette", "palette-light");
-    primaryLink.setAttribute("aria-label", primaryCtaLabel.textContent.trim());
-    primaryLink.setAttribute("href", primaryCtaHref.textContent.trim());
+  if (primaryCtaLink && primaryCtaLabel) {
+    const primaryCtaAnchor = document.createElement("a");
+    primaryCtaAnchor.classList.add("cta", "cta__secondary", "primaryCta");
+    primaryCtaAnchor.setAttribute("target", "_self");
+    primaryCtaAnchor.setAttribute("data-palette", "palette-light");
+    primaryCtaAnchor.href = primaryCtaLink.href || "#";
+    primaryCtaAnchor.setAttribute(
+      "aria-label",
+      primaryCtaLabel.textContent.trim()
+    );
 
-    const span = document.createElement("span");
-    span.classList.add("cta__label");
-    span.textContent = primaryCtaLabel.textContent.trim();
-    moveInstrumentation(primaryCtaLabel, span);
-    moveInstrumentation(primaryCtaHref, primaryLink);
-
-    primaryLink.append(span);
-    ctasDiv.append(primaryLink);
+    const primaryCtaSpan = document.createElement("span");
+    primaryCtaSpan.classList.add("cta__label");
+    primaryCtaSpan.append(...primaryCtaLabel.childNodes);
+    moveInstrumentation(primaryCtaLabel, primaryCtaSpan);
+    primaryCtaAnchor.append(primaryCtaSpan);
+    moveInstrumentation(primaryCtaLink, primaryCtaAnchor);
+    ctasDiv.append(primaryCtaAnchor);
   }
 
   const chevronWrapper = document.createElement("div");
@@ -150,41 +137,46 @@ export default function decorate(block) {
   chevronButton.setAttribute("aria-label", "Open video modal");
   chevronWrapper.append(chevronButton);
 
-  if (secondaryCtaLabel && secondaryCtaHref) {
-    const secondaryLink = document.createElement("a");
-    secondaryLink.classList.add("cta", "cta__link", "secondaryCta");
-    secondaryLink.setAttribute("target", "_self");
-    secondaryLink.setAttribute("data-palette", "palette-light");
-    secondaryLink.setAttribute(
+  if (secondaryCtaLink && secondaryCtaLabel) {
+    const secondaryCtaAnchor = document.createElement("a");
+    secondaryCtaAnchor.classList.add("cta", "cta__link", "secondaryCta");
+    secondaryCtaAnchor.setAttribute("target", "_self");
+    secondaryCtaAnchor.setAttribute("data-palette", "palette-light");
+    secondaryCtaAnchor.href = secondaryCtaLink.href || "#";
+    secondaryCtaAnchor.setAttribute(
       "aria-label",
       secondaryCtaLabel.textContent.trim()
     );
-    secondaryLink.setAttribute("href", secondaryCtaHref.textContent.trim());
 
-    const iconSpan = document.createElement("span");
-    iconSpan.classList.add("cta__icon", "qd-icon", "qd-icon--cheveron-right");
-    iconSpan.setAttribute("aria-hidden", "true");
+    const secondaryCtaIcon = document.createElement("span");
+    secondaryCtaIcon.classList.add(
+      "cta__icon",
+      "qd-icon",
+      "qd-icon--cheveron-right"
+    );
+    secondaryCtaIcon.setAttribute("aria-hidden", "true");
+    secondaryCtaAnchor.append(secondaryCtaIcon);
 
-    const labelSpan = document.createElement("span");
-    labelSpan.classList.add("cta__label");
-    labelSpan.textContent = secondaryCtaLabel.textContent.trim();
-    moveInstrumentation(secondaryCtaLabel, labelSpan);
-    moveInstrumentation(secondaryCtaHref, secondaryLink);
-
-    secondaryLink.append(iconSpan, labelSpan);
-    chevronWrapper.append(secondaryLink);
+    const secondaryCtaSpan = document.createElement("span");
+    secondaryCtaSpan.classList.add("cta__label");
+    secondaryCtaSpan.append(...secondaryCtaLabel.childNodes);
+    moveInstrumentation(secondaryCtaLabel, secondaryCtaSpan);
+    secondaryCtaAnchor.append(secondaryCtaSpan);
+    moveInstrumentation(secondaryCtaLink, secondaryCtaAnchor);
+    chevronWrapper.append(secondaryCtaAnchor);
   }
 
-  // Modal Dialog (hardcoded structure as it's not authored)
   const dialog = document.createElement("dialog");
   dialog.classList.add("hero-full-width__content--modal");
   dialog.id = "home-page-video-dialog";
   dialog.setAttribute("closedby", "any");
   dialog.setAttribute("aria-modal", "true");
   dialog.setAttribute("aria-label", "Video Modal");
+  contentDiv.append(dialog);
 
   const form = document.createElement("form");
   form.setAttribute("method", "dialog");
+  dialog.append(form);
 
   const closeButton = document.createElement("button");
   closeButton.classList.add("hero-full-width__content--modal__close-button");
@@ -192,31 +184,47 @@ export default function decorate(block) {
   closeButton.setAttribute("tabindex", "0");
   closeButton.textContent = "X";
   form.append(closeButton);
-  dialog.append(form);
 
   const videoModalDiv = document.createElement("div");
   videoModalDiv.classList.add(
     "video",
     "hero-full-width__content--modal__video"
   );
+  dialog.append(videoModalDiv);
 
   const videoContainer = document.createElement("div");
   videoContainer.classList.add("video-container", "show-controls");
+  videoModalDiv.append(videoContainer);
 
   const controlsDiv = document.createElement("div");
   controlsDiv.classList.add("video-container__controls");
+  videoContainer.append(controlsDiv);
 
   const timerDiv = document.createElement("div");
   timerDiv.classList.add("video-container__controls__timer");
+  controlsDiv.append(timerDiv);
 
   const progressArea = document.createElement("div");
   progressArea.classList.add("video-container__controls__timer__progress-area");
-  progressArea.innerHTML = `
-      <span class="video-container__controls__timer__progress-area__progress-bar"></span>
-      <span class="video-container__controls__timer__progress-area__pointer"></span>
-      <span class="video-container__controls__timer__progress-area__progress-pending"></span>
-    `;
   timerDiv.append(progressArea);
+
+  const progressBar = document.createElement("span");
+  progressBar.classList.add(
+    "video-container__controls__timer__progress-area__progress-bar"
+  );
+  progressArea.append(progressBar);
+
+  const pointer = document.createElement("span");
+  pointer.classList.add(
+    "video-container__controls__timer__progress-area__pointer"
+  );
+  progressArea.append(pointer);
+
+  const progressPending = document.createElement("span");
+  progressPending.classList.add(
+    "video-container__controls__timer__progress-area__progress-pending"
+  );
+  progressArea.append(progressPending);
 
   const currentTime = document.createElement("p");
   currentTime.classList.add("video-container__controls__timer__current-time");
@@ -227,47 +235,70 @@ export default function decorate(block) {
   duration.classList.add("video-container__controls__timer__duration");
   duration.textContent = "00:00";
   timerDiv.append(duration);
-  controlsDiv.append(timerDiv);
 
   const buttonsDiv = document.createElement("div");
   buttonsDiv.classList.add("video-container__controls__buttons");
-  buttonsDiv.innerHTML = `
-      <button class="video-container__controls__buttons__play-button video-container__controls__buttons--button">
-        <span class="video-container__controls__buttons__icon qd-icon qd-icon--play"></span>
-      </button>
-      <button class="video-container__controls__buttons__mute-button video-container__controls__buttons--button">
-        <span class="video-container__controls__buttons__icon qd-icon qd-icon--volume"></span>
-      </button>
-      <button class="video-container__controls__buttons__fullscreen-button video-container__controls__buttons--button">
-        <span class="video-container__controls__buttons__icon qd-icon qd-icon--fullscreen"></span>
-      </button>
-    `;
   controlsDiv.append(buttonsDiv);
-  videoContainer.append(controlsDiv);
 
-  const modalVideo = document.createElement("video");
-  modalVideo.classList.add("video-container__video");
-  modalVideo.setAttribute("playsinline", "");
-  modalVideo.setAttribute("webkit-playsinline", "");
-  modalVideo.setAttribute("muted", "true");
-  modalVideo.setAttribute("autoplay", "");
+  const playButton = document.createElement("button");
+  playButton.classList.add(
+    "video-container__controls__buttons__play-button",
+    "video-container__controls__buttons--button"
+  );
+  buttonsDiv.append(playButton);
 
-  if (backgroundVideoSource) {
-    const modalSource = document.createElement("source");
-    modalSource.setAttribute("src", backgroundVideoSource.textContent.trim());
-    modalSource.setAttribute("type", "video/mp4");
-    // No moveInstrumentation here as it's a duplicate source for the modal
-    modalVideo.append(modalSource);
+  const playIcon = document.createElement("span");
+  playIcon.classList.add(
+    "video-container__controls__buttons__icon",
+    "qd-icon",
+    "qd-icon--play"
+  );
+  playButton.append(playIcon);
+
+  const muteButton = document.createElement("button");
+  muteButton.classList.add(
+    "video-container__controls__buttons__mute-button",
+    "video-container__controls__buttons--button"
+  );
+  buttonsDiv.append(muteButton);
+
+  const muteIcon = document.createElement("span");
+  muteIcon.classList.add(
+    "video-container__controls__buttons__icon",
+    "qd-icon",
+    "qd-icon--volume"
+  );
+  muteButton.append(muteIcon);
+
+  const fullscreenButton = document.createElement("button");
+  fullscreenButton.classList.add(
+    "video-container__controls__buttons__fullscreen-button",
+    "video-container__controls__buttons--button"
+  );
+  buttonsDiv.append(fullscreenButton);
+
+  const fullscreenIcon = document.createElement("span");
+  fullscreenIcon.classList.add(
+    "video-container__controls__buttons__icon",
+    "qd-icon",
+    "qd-icon--fullscreen"
+  );
+  fullscreenButton.append(fullscreenIcon);
+
+  const modalVideoElem = document.createElement("video");
+  modalVideoElem.classList.add("video-container__video");
+  modalVideoElem.setAttribute("playsinline", "");
+  modalVideoElem.setAttribute("webkit-playsinline", "");
+  modalVideoElem.setAttribute("muted", "true");
+  modalVideoElem.setAttribute("autoplay", "");
+  modalVideoElem.setAttribute("x-webkit-airplay", "allow");
+
+  if (backgroundVideo) {
+    const modalSourceElem = document.createElement("source");
+    modalSourceElem.setAttribute("type", "video/mp4");
+    modalSourceElem.src =
+      backgroundVideo.href || backgroundVideo.textContent.trim();
+    modalVideoElem.append(modalSourceElem);
   }
-  modalVideo.append("webkit-playsinline", 'x-webkit-airplay="allow"'); // These are text nodes in the original HTML
-  videoContainer.append(modalVideo);
-  videoModalDiv.append(videoContainer);
-  dialog.append(videoModalDiv);
-
-  contentDiv.append(dialog);
-
-  // Set aria-label for the block based on the title content
-  if (title) {
-    block.setAttribute("aria-label", title.outerHTML);
-  }
+  videoContainer.append(modalVideoElem);
 }
