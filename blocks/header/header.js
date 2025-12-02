456fbfce0e9194b29a7851dfe9fb93acccd10c25
fansan6190 <fanindra.saini@axeno.co>
2025-12-02 01:52:39 -0800

    boingweekly-siddhant

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,183 +1,166 @@
-//Header
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
 
-export default function decorate(block) {
-  const headerContainer = document.createElement('section');
-  headerContainer.classList.add('header-container');
-  moveInstrumentation(block, headerContainer);
+// media query match that indicates mobile/tablet width
+const isDesktop = window.matchMedia('(min-width: 900px)');
 
-  const homeLink = block.querySelector('[data-aue-prop="homeLink"]');
-  const logoImage = block.querySelector('[data-aue-prop="logoImage"] img');
-  if (homeLink && logoImage) {
-    const homeAnchor = document.createElement('a');
-    homeAnchor.rel = 'no-follow';
-    homeAnchor.title = 'home';
-    homeAnchor.href = homeLink.href;
-    homeAnchor.append(createOptimizedPicture(logoImage.src, logoImage.alt));
-    moveInstrumentation(homeLink, homeAnchor);
-    moveInstrumentation(logoImage, homeAnchor.querySelector('img'));
-    headerContainer.append(homeAnchor);
-  } else if (homeLink) {
-    const homeAnchor = document.createElement('a');
-    homeAnchor.rel = 'no-follow';
-    homeAnchor.title = 'home';
-    homeAnchor.href = homeLink.href;
-    homeAnchor.append(...homeLink.childNodes);
-    moveInstrumentation(homeLink, homeAnchor);
-    headerContainer.append(homeAnchor);
-  } else if (logoImage) {
-    const homeAnchor = document.createElement('a');
-    homeAnchor.rel = 'no-follow';
-    homeAnchor.title = 'home';
-    homeAnchor.href = '/'; // Default if no link is provided
-    homeAnchor.append(createOptimizedPicture(logoImage.src, logoImage.alt));
-    moveInstrumentation(logoImage, homeAnchor.querySelector('img'));
-    headerContainer.append(homeAnchor);
+function closeOnEscape(e) {
+  if (e.code === 'Escape') {
+    const nav = document.getElementById('nav');
+    const navSections = nav.querySelector('.nav-sections');
+    const navSectionExpanded = navSections.querySelector('[aria-expanded="true"]');
+    if (navSectionExpanded && isDesktop.matches) {
+      // eslint-disable-next-line no-use-before-define
+      toggleAllNavSections(navSections);
+      navSectionExpanded.focus();
+    } else if (!isDesktop.matches) {
+      // eslint-disable-next-line no-use-before-define
+      toggleMenu(nav, navSections);
+      nav.querySelector('button').focus();
+    }
+  }
+}
+
+function closeOnFocusLost(e) {
+  const nav = e.currentTarget;
+  if (!nav.contains(e.relatedTarget)) {
+    const navSections = nav.querySelector('.nav-sections');
+    const navSectionExpanded = navSections.querySelector('[aria-expanded="true"]');
+    if (navSectionExpanded && isDesktop.matches) {
+      // eslint-disable-next-line no-use-before-define
+      toggleAllNavSections(navSections, false);
+    } else if (!isDesktop.matches) {
+      // eslint-disable-next-line no-use-before-define
+      toggleMenu(nav, navSections, false);
+    }
+  }
+}
+
+function openOnKeydown(e) {
+  const focused = document.activeElement;
+  const isNavDrop = focused.className === 'nav-drop';
+  if (isNavDrop && (e.code === 'Enter' || e.code === 'Space')) {
+    const dropExpanded = focused.getAttribute('aria-expanded') === 'true';
+    // eslint-disable-next-line no-use-before-define
+    toggleAllNavSections(focused.closest('.nav-sections'));
+    focused.setAttribute('aria-expanded', dropExpanded ? 'false' : 'true');
+  }
+}
+
+function focusNavSection() {
+  document.activeElement.addEventListener('keydown', openOnKeydown);
+}
+
+/**
+ * Toggles all nav sections
+ * @param {Element} sections The container element
+ * @param {Boolean} expanded Whether the element should be expanded or collapsed
+ */
+function toggleAllNavSections(sections, expanded = false) {
+  sections.querySelectorAll('.nav-sections .default-content-wrapper > ul > li').forEach((section) => {
+    section.setAttribute('aria-expanded', expanded);
+  });
+}
+
+/**
+ * Toggles the entire nav
+ * @param {Element} nav The container element
+ * @param {Element} navSections The nav sections within the container element
+ * @param {*} forceExpanded Optional param to force nav expand behavior when not null
+ */
+function toggleMenu(nav, navSections, forceExpanded = null) {
+  const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
+  const button = nav.querySelector('.nav-hamburger button');
+  document.body.style.overflowY = (expanded || isDesktop.matches) ? '' : 'hidden';
+  nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  toggleAllNavSections(navSections, expanded || isDesktop.matches ? 'false' : 'true');
+  button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
+  // enable nav dropdown keyboard accessibility
+  const navDrops = navSections.querySelectorAll('.nav-drop');
+  if (isDesktop.matches) {
+    navDrops.forEach((drop) => {
+      if (!drop.hasAttribute('tabindex')) {
+        drop.setAttribute('tabindex', 0);
+        drop.addEventListener('focus', focusNavSection);
+      }
+    });
+  } else {
+    navDrops.forEach((drop) => {
+      drop.removeAttribute('tabindex');
+      drop.removeEventListener('focus', focusNavSection);
+    });
   }
 
-  const headerNav = document.createElement('div');
-  headerNav.classList.add('header-nav');
-  headerContainer.append(headerNav);
+  // enable menu collapse on escape keypress
+  if (!expanded || isDesktop.matches) {
+    // collapse menu on escape press
+    window.addEventListener('keydown', closeOnEscape);
+    // collapse menu on focus lost
+    nav.addEventListener('focusout', closeOnFocusLost);
+  } else {
+    window.removeEventListener('keydown', closeOnEscape);
+    nav.removeEventListener('focusout', closeOnFocusLost);
+  }
+}
 
-  const aboutLink = block.querySelector('[data-aue-prop="aboutLink"]');
-  if (aboutLink) {
-    const aboutAnchor = document.createElement('a');
-    aboutAnchor.href = aboutLink.href;
-    aboutAnchor.append(...aboutLink.childNodes);
-    moveInstrumentation(aboutLink, aboutAnchor);
-    headerNav.append(aboutAnchor);
+/**
+ * loads and decorates the header, mainly the nav
+ * @param {Element} block The header block element
+ */
+export default async function decorate(block) {
+  // load nav as fragment
+  const navMeta = getMetadata('nav');
+  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const fragment = await loadFragment(navPath);
+
+  // decorate nav DOM
+  block.textContent = '';
+  const nav = document.createElement('nav');
+  nav.id = 'nav';
+  while (fragment.firstElementChild) nav.append(fragment.firstElementChild);
+
+  const classes = ['brand', 'sections', 'tools'];
+  classes.forEach((c, i) => {
+    const section = nav.children[i];
+    if (section) section.classList.add(`nav-${c}`);
+  });
+
+  const navBrand = nav.querySelector('.nav-brand');
+  const brandLink = navBrand.querySelector('.button');
+  if (brandLink) {
+    brandLink.className = '';
+    brandLink.closest('.button-container').className = '';
   }
 
-  const experiencesLink = block.querySelector('[data-aue-prop="experiencesLink"]');
-  if (experiencesLink) {
-    const experiencesAnchor = document.createElement('a');
-    experiencesAnchor.href = experiencesLink.href;
-    experiencesAnchor.append(...experiencesLink.childNodes);
-    moveInstrumentation(experiencesLink, experiencesAnchor);
-    headerNav.append(experiencesAnchor);
+  const navSections = nav.querySelector('.nav-sections');
+  if (navSections) {
+    navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
+      if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+      navSection.addEventListener('click', () => {
+        if (isDesktop.matches) {
+          const expanded = navSection.getAttribute('aria-expanded') === 'true';
+          toggleAllNavSections(navSections);
+          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        }
+      });
+    });
   }
 
-  const requestInviteLink = block.querySelector('[data-aue-prop="requestInviteLink"]');
-  if (requestInviteLink) {
-    const requestInviteAnchor = document.createElement('a');
-    requestInviteAnchor.classList.add('header-request-invite');
-    requestInviteAnchor.href = requestInviteLink.href;
-    requestInviteAnchor.append(...requestInviteLink.childNodes);
-    moveInstrumentation(requestInviteLink, requestInviteAnchor);
-    headerNav.append(requestInviteAnchor);
-  }
+  // hamburger for mobile
+  const hamburger = document.createElement('div');
+  hamburger.classList.add('nav-hamburger');
+  hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
+      <span class="nav-hamburger-icon"></span>
+    </button>`;
+  hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
+  nav.prepend(hamburger);
+  nav.setAttribute('aria-expanded', 'false');
+  // prevent mobile nav behavior on window resize
+  toggleMenu(nav, navSections, isDesktop.matches);
+  isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
 
-  const instagramLink = block.querySelector('[data-aue-prop="instagramLink"]');
-  const instagramIcon = block.querySelector('[data-aue-prop="instagramIcon"] img');
-  if (instagramLink && instagramIcon) {
-    const instagramAnchor = document.createElement('a');
-    instagramAnchor.classList.add('header-insta-icon');
-    instagramAnchor.target = '_blank';
-    instagramAnchor.href = instagramLink.href;
-    instagramAnchor.append(createOptimizedPicture(instagramIcon.src, instagramIcon.alt));
-    moveInstrumentation(instagramLink, instagramAnchor);
-    moveInstrumentation(instagramIcon, instagramAnchor.querySelector('img'));
-    headerNav.append(instagramAnchor);
-  } else if (instagramLink) {
-    const instagramAnchor = document.createElement('a');
-    instagramAnchor.classList.add('header-insta-icon');
-    instagramAnchor.target = '_blank';
-    instagramAnchor.href = instagramLink.href;
-    instagramAnchor.append(...instagramLink.childNodes);
-    moveInstrumentation(instagramLink, instagramAnchor);
-    headerNav.append(instagramAnchor);
-  } else if (instagramIcon) {
-    const instagramAnchor = document.createElement('a');
-    instagramAnchor.classList.add('header-insta-icon');
-    instagramAnchor.target = '_blank';
-    instagramAnchor.href = '#'; // Default if no link is provided
-    instagramAnchor.append(createOptimizedPicture(instagramIcon.src, instagramIcon.alt));
-    moveInstrumentation(instagramIcon, instagramAnchor.querySelector('img'));
-    headerNav.append(instagramAnchor);
-  }
-
-  const headerMenu = document.createElement('div');
-  headerMenu.classList.add('header-menu');
-  headerContainer.append(headerMenu);
-
-  for (let i = 1; i <= 3; i += 1) {
-    const line = document.createElement('div');
-    line.classList.add('header-menuline', `header-line-${i}`);
-    headerMenu.append(line);
-  }
-
-  const headerPhoneMenu = document.createElement('section');
-  headerPhoneMenu.classList.add('header-phone-menu');
-
-  const phoneMenuBackImage = block.querySelector('[data-aue-prop="phoneMenuBackImage"] img');
-  if (phoneMenuBackImage) {
-    const pic = createOptimizedPicture(phoneMenuBackImage.src, phoneMenuBackImage.alt);
-    pic.classList.add('header-phone-menu-back');
-    moveInstrumentation(phoneMenuBackImage, pic.querySelector('img'));
-    headerPhoneMenu.append(pic);
-  }
-
-  if (aboutLink) { // Re-using existing aboutLink for phone menu
-    const aboutAnchor = document.createElement('a');
-    aboutAnchor.href = aboutLink.href;
-    aboutAnchor.append(...aboutLink.childNodes);
-    moveInstrumentation(aboutLink, aboutAnchor);
-    headerPhoneMenu.append(aboutAnchor);
-  }
-
-  if (experiencesLink) { // Re-using existing experiencesLink for phone menu
-    const experiencesAnchor = document.createElement('a');
-    experiencesAnchor.href = experiencesLink.href;
-    experiencesAnchor.append(...experiencesLink.childNodes);
-    moveInstrumentation(experiencesLink, experiencesAnchor);
-    headerPhoneMenu.append(experiencesAnchor);
-  }
-
-  if (requestInviteLink) { // Re-using existing requestInviteLink for phone menu
-    const requestInviteAnchor = document.createElement('a');
-    requestInviteAnchor.classList.add('header-request-invite');
-    requestInviteAnchor.href = requestInviteLink.href;
-    requestInviteAnchor.append(...requestInviteLink.childNodes);
-    moveInstrumentation(requestInviteLink, requestInviteAnchor);
-    headerPhoneMenu.append(requestInviteAnchor);
-  }
-
-  if (instagramLink && instagramIcon) { // Re-using existing instagramLink and instagramIcon for phone menu
-    const instagramAnchor = document.createElement('a');
-    instagramAnchor.classList.add('header-insta-icon');
-    instagramAnchor.target = '_blank';
-    instagramAnchor.href = instagramLink.href;
-    instagramAnchor.append(createOptimizedPicture(instagramIcon.src, instagramIcon.alt));
-    moveInstrumentation(instagramLink, instagramAnchor);
-    moveInstrumentation(instagramIcon, instagramAnchor.querySelector('img'));
-    headerPhoneMenu.append(instagramAnchor);
-  } else if (instagramLink) {
-    const instagramAnchor = document.createElement('a');
-    instagramAnchor.classList.add('header-insta-icon');
-    instagramAnchor.target = '_blank';
-    instagramAnchor.href = instagramLink.href;
-    instagramAnchor.append(...instagramLink.childNodes);
-    moveInstrumentation(instagramLink, instagramAnchor);
-    headerPhoneMenu.append(instagramAnchor);
-  } else if (instagramIcon) {
-    const instagramAnchor = document.createElement('a');
-    instagramAnchor.classList.add('header-insta-icon');
-    instagramAnchor.target = '_blank';
-    instagramAnchor.href = '#'; // Default if no link is provided
-    instagramAnchor.append(createOptimizedPicture(instagramIcon.src, instagramIcon.alt));
-    moveInstrumentation(instagramIcon, instagramAnchor.querySelector('img'));
-    headerPhoneMenu.append(instagramAnchor);
-  }
-
-  const phoneMenuLogo = block.querySelector('[data-aue-prop="phoneMenuLogo"] img');
-  if (phoneMenuLogo) {
-    const pic = createOptimizedPicture(phoneMenuLogo.src, phoneMenuLogo.alt);
-    pic.classList.add('header-phone-menu-logo');
-    moveInstrumentation(phoneMenuLogo, pic.querySelector('img'));
-    headerPhoneMenu.append(pic);
-  }
-
-  block.innerHTML = '';
-  block.append(headerContainer, headerPhoneMenu);
+  const navWrapper = document.createElement('div');
+  navWrapper.className = 'nav-wrapper';
+  navWrapper.append(nav);
+  block.append(navWrapper);
 }
