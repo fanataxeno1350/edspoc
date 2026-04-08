9bfcddd02958cc9534fb1ec36ae8944e1aa04649
fansan6190 <fanindra.saini@axeno.co>
2026-04-08 11:33:02 +0530

    script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -115,13 +115,13 @@ async function loadEager(doc) {
  */
 async function loadLazy(doc) {
   const main = doc.querySelector('main');
+  loadHeader(doc.querySelector('header'));
   await loadSections(main);
 
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
-
-  loadHeader(doc.querySelector('header'));
+  
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
